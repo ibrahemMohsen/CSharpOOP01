5e601aba977cdc8515fef86ba3d47c8c0302aef0
ibrahemMohsen <ibrahemmohsen851@gmail.com>
2026-09-13 12:53:56 +0300

    Question01

--- CSharpOOP01/Program.cs
+++ CSharpOOP01/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            
+            #region Part01
+
+            #region Question01
+            // a) The original copy stays the same beause it was copied by value
+
+            // b) both variables get modified because it was copied by reference
+            #endregion
+
+            #endregion
         }
     }
 }
