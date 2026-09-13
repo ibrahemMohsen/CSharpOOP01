61611ea8a2f67b6c5e2236e3aa1eb2255593687b
ibrahemMohsen <ibrahemmohsen851@gmail.com>
2026-09-13 12:54:20 +0300

    Question02

--- CSharpOOP01/Program.cs
+++ CSharpOOP01/Program.cs
@@ -12,6 +12,14 @@
             // b) both variables get modified because it was copied by reference
             #endregion
 
+            #region Question02
+            // a) 1.all fields are public, so they can easily be modified from outside.
+            // 2.Weight can be assignd zero or a negative number without validation.
+            // 3.DeliveryFee can be assigned zero or a negative number without validation.
+
+            // b) By enhancing encapsulation and only allowing valid and intended operations on the data
+            #endregion
+
             #endregion
         }
     }
