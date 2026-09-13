e919284af4372229854be2459da7e88cc900b853
ibrahemMohsen <ibrahemmohsen851@gmail.com>
2026-09-13 12:59:19 +0300

    Part02 Question01

---
+++ SmartDeliveryManagementSystem/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace SmartDeliveryManagementSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DeliveryAddress deliveryAddress = new DeliveryAddress("Assiut", "Al-Salam", 12);
+            DeliveryAddress deliveryAddressCopy = deliveryAddress;
+            deliveryAddressCopy.BuildingNumber = 5;
+            deliveryAddressCopy.Street = "Dar Heraa'";
+
+
+            Console.WriteLine(deliveryAddress.GetFullAddress());
+
+            Console.WriteLine(deliveryAddressCopy.GetFullAddress());
+        }
+    }
+}
